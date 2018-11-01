f476087354494d539d6cdbe1def59267f12383d3
Jason Crescenzo <crescenzojay@gmail.com>
2018-11-01 17:56:44 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,15 @@
 class Project
   
-   attr_accessor :title
+   attr_accessor :title, :backers 
   
-  @@backers = []
   
   def initialize (title)
     @title = title
+    @backers = []
   end 
   
    def add_backer (backer)
-     @@backers << backer
+     @backers << backer
    end 
    
 end 
